892dda3e998395d4fb8726a80258044f25bba258
iXploitID <93501879+iXploitID@users.noreply.github.com>
2021-11-03 16:20:33 +0700

    Add files via upload
    termux-setup-storage  JAWAB y  lalu klik enter  :V
    pkg update && pkg upgrade
    pkg install bash
    pkg install git
    git clone https://github.com/iXploitID/KTPV2
    cd KTPV2
    bash y.sh
    
    
    OH IYA BTW YANG MANGSUT SD CARD PENYIMPANAN INTERNAL :V JADI CARI AJA NAMA FILE NYA KTP+KK.txt

---
+++ y.sh
@@ -0,0 +1,56 @@
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+
+
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+
+
+
+echo -e "CODED BY :"
+echo -e $red"    ██     ▄▄▄  ▄▄▄            ▄▄▄▄                   ██       ██"
+echo -e $red"    ▀▀      ██▄▄██             ▀▀██                   ▀▀       ██"
+echo -e $red"  ████       ████    ██▄███▄     ██       ▄████▄    ████     ███████"
+echo -e $red"    ██        ██     ██▀  ▀██    ██      ██▀  ▀██     ██       ██"
+echo -e $white"    ██       ████    ██    ██    ██      ██    ██     ██       ██"
+echo -e $white" ▄▄▄██▄▄▄   ██  ██   ███▄▄██▀    ██▄▄▄   ▀██▄▄██▀  ▄▄▄██▄▄▄    ██▄▄▄"
+echo -e $white" ▀▀▀▀▀▀▀▀  ▀▀▀  ▀▀▀  ██ ▀▀▀       ▀▀▀▀     ▀▀▀▀    ▀▀▀▀▀▀▀▀     ▀▀▀▀"
+echo -e $white"                     ██"
+echo
+echo
+echo
+echo -e $red "################################"
+echo -e $red "# CREATOR: i-Xploit            #"
+echo -e $red "# Yt: Ramdom Toturial          #"
+echo -e $red "# Git hub :                    #"
+echo -e $white "#Link yt :                    #"
+echo -e $white "#jangan recrod script ini     #"
+echo -e $white "###############################"
+echo
+echo
+echo
+echo
+echo -e $blue"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "1.LANJUT KE SC KTP+KK GRATIS NYA :V"
+echo -e $blue"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo -e "2.Cek yt creator"
+echo -e $blue"▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀"
+echo
+echo
+echo
+read -p "[Root@i-Xploit~# " a;
+
+if [ $a = 1 ] || [ $a = 1 ]
+then
+bash KTPV2.sh
+fi
+
+
+if [ $a =  2 ] || [ $a = 2 ]
+then
+xdg-open https://youtube.com/channel/UCs-HYlgurVWTMobpOPXxiKg
+fi
